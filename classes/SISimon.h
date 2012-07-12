3228593206f0ae9ca691fb87b414113fcad35e49
Derek Clarkson <d4rkf1br3@gmail.com>
2012-07-12 18:16:56 +1000

    Moving classes to core

--- classes/SISimon.h
+++ classes/SISimon.h
@@ -5,7 +5,7 @@
 //  Created by Derek Clarkson on 6/18/11.
 //  Copyright 2011. All rights reserved.
 //
-#import "SIConstants.h"
+#import <Simon-core/SIConstants.h>
 #import "SIMacros.h"
 #import "SIUIConstants.h"
 #import "SIStepMapping.h"
